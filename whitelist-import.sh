015bb67ec2d7581bb860d3481f6e8b5ec8bcc933
==TIM.©.B  == <gummiflip@outlook.de>
2025-07-06 23:49:57 +0200

    Update whitelist-import.sh

--- whitelist-import.sh
+++ whitelist-import.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Pfad zur Whitelist
-WL_URL="https://raw.githubusercontent.com/TimInTech/Whitelist/main/Whitelist.txt"
+WL_URL="https://raw.githubusercontent.com/TimInTech/Whitelist/main/Whitelist.final.personal.txt"
 WL_FILE="/tmp/Whitelist.txt"
 
-# Prüfe, ob Pi-hole installiert ist
 if ! command -v pihole &> /dev/null; then
   echo "Pi-hole not found. Exiting."
   exit 1
 fi
 
-# Download der Whitelist
 echo "[+] Downloading whitelist from: $WL_URL"
 wget -q -O "$WL_FILE" "$WL_URL"
 
@@ -19,11 +16,9 @@ if [ $? -ne 0 ] || [ ! -s "$WL_FILE" ]; then
   exit 2
 fi
 
-# Optional: Bisherige Einträge entfernen
 echo "[+] Removing previous whitelist entries (optional)..."
 pihole allow --nuke
 
-# Importiere alle Domains
 echo "[+] Importing entries..."
 xargs -a "$WL_FILE" -r -I {} pihole allow "{}"
 
